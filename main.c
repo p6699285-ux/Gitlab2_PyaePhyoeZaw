328146026dde55a482f9803592a58d2f03ed658d
Pyae Phyoe <p6699285@gmail.com>
2026-09-23 15:38:45 +0300

    Added name input

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
 int main(int argc, char **argv)
- {
-	print("Hello world!\n");
- 	return 0;
- }
+{
+    char name[50];
+
+    printf("Enter your name: ");
+    scanf("%49s", name);
+
+    printf("Hello, %s!\n", name);
+
+    return 0;
+}
